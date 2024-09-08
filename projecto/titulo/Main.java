5cc4df930125833d7a9cc4b518ba2f246d3b89e1
Alejandro Gallegos <l24120333@morelia.tecnm.mx>
2024-09-07 21:49:47 -0600

    Comentario

---
+++ projecto/titulo/Main.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+
+public class Main{
+	public static void main(String args[]){
+	    Scanner read = new Scanner(System.in);
+
+	    System.out.println("Ingresar cadena:");
+       String cadena= read.nextString();
+
+       if("(",")","[""]","{""}" ){
+       for(int i=0; i>0;i++){
+
+       }
+       }
+
+
+	}
+}
